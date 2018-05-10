eb500d69e4b5d03032f59060dbf68369ca565344
Theodore Huynh <theodore.huynh.melb@gmail.com>
2018-05-10 19:06:33 +1000

    Finish trace mode

--- simulation.py
+++ simulation.py
@@ -9,8 +9,136 @@
 #delayedoff_time #Tc
 #time_end #stops the simulation if the master clock exceeds this value
 
+
+import heapq
+
+
+
+def find_longest_server_type(servers,events,type):
+    final_id=None
+    for id in servers:
+        if servers[id]['status']==type:
+            if (final_id==None):
+                final_id= id
+            else:
+                if (find_server_time_type(events,id,type) > find_server_time_type(events,final_id,type)):
+                    final_id = id
+    return final_id
+
+def find_server_time_type(events,server_id,type):
+    for [time,event] in events:
+        if (event['type']==type) and (event['id']==server_id):
+            return time
+    return None
+
+def remove_event(events,type,id):
+    for [time,event] in events:
+        if (event['type']==type and event['id']==id):
+            events.remove([time,event])
+            heapq.heapify(events)
+
+def wait_for_server(clock,servers,queue,events,job_id,setup_time):
+    for id in servers:
+        if servers[id]['status']=='off':
+            servers[id]['status']='setup'
+            heapq.heappush(events, [clock + setup_time, {'type':'setup', 'id':id}    ] )
+            queue.append({'id':job_id,'status':'MARKED'})
+            return 'MARKED'
+
+    #all server must be either busy or setup
+    queue.append({'id':job_id,'status':'UNMARKED'})
+    return 'UNMARKED'
+
+def use_delay_server(clock,servers,jobs,events,server_id,job_id):
+    remove_event(events,'delay',server_id)
+    servers[server_id]['status']='busy'
+    heapq.heappush(events, [clock+jobs[job_id]['service'], {'type':'departure', 'id':[server_id,job_id]} ] )
+
+def use_free_server(clock,servers,jobs,events,server_id,job_id):
+    servers[server_id]['status']='busy'
+    heapq.heappush(events, [clock+jobs[job_id]['service'], {'type':'departure', 'id':[server_id,job_id]} ] )
+
+def pop_first_marked(queue):
+    for job in queue:
+        if job['status']=='MARKED':
+            queue.remove(job)
+            return job
+
 def simulation(mode, arrival, service, m, setup_time, delayedoff_time, time_end="Na"):
-    master_clock = 0.0
+    clock = 0.0
+    total_response_time=0.0
+
+    n_jobs = len(arrival)
+    departures=[]
+
+    servers = {}
+    jobs = {}
+    queue = []
+    events = []
     
+    
+    
+    
+    # events { time : ? ,type: "arrive, depart, setup, delay"}
+
+    # initialize servers
+    for i in range(1,m+1):
+        servers[i]={'status':'off'}  #server status based on server its id (off,busy,setup,delay)
+    
+    # initialize jobs
+    for i in range(n_jobs):
+        jobs[i+1]={"arrival":arrival[i],"service":service[i]}
+    
+    # initialize arrival events
+    for id in jobs:
+        heapq.heappush(events, [jobs[id]['arrival'], {'type':'arrival', 'id':id}    ] )
+
+    print(servers)
+    print(jobs)
+    print(events)
+    
+    while len(events)!=0:
+        [time,event]=heapq.heappop(events)
+        clock=time
+        if event['type']=='arrival':
+            job_id = event['id']
+            free_server_id = find_longest_server_type(servers,events,'delay')
+            if free_server_id==None:
+                wait_for_server(clock,servers,queue,events,job_id,setup_time)
+            else:
+                use_delay_server(clock,servers,jobs,events,free_server_id,job_id)
+        
+        if event['type']=='departure':
+            [servers_id,job_id] = event['id']
+            departures.append(clock)
+            total_response_time += clock - jobs[job_id]['arrival']
+
+            if len(queue)==0:
+                servers[servers_id]['status']='delay'
+                heapq.heappush(events, [clock+delayedoff_time, {'type':'delay', 'id':server_id} ] )
+            else:
+                job = queue.pop(0)
+                use_free_server(clock,servers,jobs,events,server_id,job['id'])
+                if job['status']=='MARKED':
+                    setup_server_id = find_longest_server_type(servers,events,'setup')
+                    servers[setup_server_id]['status']='off'
+                    remove_event(events,'setup',setup_server_id)
+
+
+        if event['type']=='setup':
+            server_id=event['id']
+            job = pop_first_marked(queue)
+            use_free_server(clock,servers,jobs,events,server_id,job['id'])
+        
+        if event['type']=='delay':
+            server_id=event['id']
+            servers[server_id]['status']=='off'
     #output 2 files
+
+    mean_response_time = total_response_time/n_jobs
+    print ("Mean Response Time: {0:.3f}".format(mean_response_time))
+
+    for i in range(n_jobs):
+        print ("{0:.3f} {1:.3f}".format(arrival[i],departures[i]))
+
     pass
--- wrapper.py
+++ wrapper.py
@@ -3,25 +3,34 @@
 
 import os, simulation as si
 
-def read_file(file_name):
+
+def read_file_string(file_name):
     with open(file_name) as f:
         content = f.readlines()
     #whitespace
     content = [x.strip() for x in content] 
     return content
 
+def read_file_float(file_name):
+    with open(file_name) as f:
+        content = f.readlines()
+    #whitespace
+    content = [float(x.strip()) for x in content] 
+    return content
+
 def main():
     os.chdir(os.getcwd()+"\COMP9334-assignment2\sample_1") 
-    no_tests = int(read_file("num_tests.txt")[0])
+    no_tests = int(read_file_float("num_tests.txt")[0])
     for i in range(1,no_tests+1):
-        mode = read_file("mode_{0}.txt".format(i))[0]
-        para = read_file("para_{0}.txt".format(i))
+        mode = read_file_string("mode_{0}.txt".format(i))[0]
+        para = read_file_float("para_{0}.txt".format(i))
         m = int(para[0])
-        setup_time = float(para[1])
-        delayedoff_time = float(para[2])
+        setup_time = para[1]
+        delayedoff_time = para[2]
 
-        arrival = read_file("arrival_{0}.txt".format(i))
-        service = read_file("service_{0}.txt".format(i))
+        arrival = read_file_float("arrival_{0}.txt".format(i))
+        service = read_file_float("service_{0}.txt".format(i))
+
 
         print ("Test {0}".format(i))
         print ("Mode: {0}".format(mode))
@@ -30,9 +39,10 @@ def main():
         print ("Delayed off time: {0}".format(delayedoff_time))
         print ("Arrival time: {0}".format(arrival))
         print ("Service time: {0}".format(service))
-        print()
+        
         si.simulation(mode, arrival, service, m, setup_time, delayedoff_time)
         
+        print()
 
 if __name__ == "__main__":
     main()
